6da6ec19ce23ce99075432e5858eedb1c2dc881c
callezenwaka <callisezenwaka@gmail.com>
2021-12-02 00:37:24 +0100

    Compare ref vs reactive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,34 @@
 <template>
   <div class="home">
-    home
-    <p>My name is {{ name }} and my age is {{ age }}.</p>
-    <button @click="handleclick">click me</button>
-    <button @click="age++">Increase Age by 1</button>
-    <input type="text" v-model="name" id="">
+    <h1>Home</h1>
+    <h2>Ref</h2>
+    <p>{{ userOne.name}} - {{ userOne.age }}</p>
+    <button @click="updateUserOne">Update User One</button>
+    <h2>Reactive</h2>
+    <p>{{ userTwo.name}} - {{ userTwo.age }}</p>
+    <button @click="updateUserTwo">Update User Two</button>
   </div>
 </template>
 
 <script>
-import { ref } from '@vue/reactivity';
+import { ref, reactive } from '@vue/reactivity';
 // @ is an alias to /src
 
 export default {
   name: 'Home',
   setup() {
-    let name = ref('Carl');
-    let age = ref(20);
+    const userOne = ref({ name: 'Carl', age: 20 });
+    const userTwo = reactive({ name: 'Karl', age: 25 });
 
-    let handleclick = () => {
-      name.value = 'Karl';
-      age.value = 25;
+    const updateUserOne = () => {
+      userOne.value.age = 25;
     }
 
-    return { name, age, handleclick }
+    const updateUserTwo = () => {
+      userTwo.age = 30;
+    }
+
+    return { userOne, userTwo, updateUserOne, updateUserTwo }
   }
 }
 </script>
