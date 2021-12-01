9d6d2874caed9bc0974f805bd5478d44479de80b
callezenwaka <callisezenwaka@gmail.com>
2021-12-02 00:10:07 +0100

    Use ref attribute

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    home
+    <p ref="par">My name is {{ name }} and my age is {{ age }}.</p>
+    <button @click="handleclick">click me</button>
+  </div>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity';
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  setup() {
+    let name = 'Carl';
+    let age = 20;
+
+    let par = ref(null);
+
+    let handleclick = () => {
+      console.log(par.value);
+      par.value.classList.add('test');
+      par.value.textContent = 'hello world!';
+    }
+
+    return { name, age, handleclick, par }
+  }
+}
+</script>
