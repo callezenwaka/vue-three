8d5651f72e9792022464d4ecb3eb6e631c45688a
callezenwaka <callisezenwaka@gmail.com>
2021-12-02 03:31:37 +0100

    Use composables

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,7 +12,7 @@
 
 <script>
 import PostList from "@/components/PostList.vue";
-import { ref } from '@vue/reactivity';
+import getPosts from "@/composables/getPosts";
 // @ is an alias to /src
 
 export default {
@@ -21,22 +21,7 @@ export default {
     PostList
   },
   setup() {
-    const posts = ref([]);
-    
-    const error = ref(null);
-
-    const load = async () => {
-      try {
-        let data = await fetch('http://localhost:3000/posts');
-        if (!data.ok) {
-          throw Error('No data received!');
-        }
-        posts.value = await data.json();
-      } catch (err) {
-        error.value = err.message;
-        console.log(error.value);
-      }
-    }
+    const { posts, error, load } = getPosts();
 
     load();
 
