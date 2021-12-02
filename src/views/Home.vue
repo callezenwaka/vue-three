033f0ef8beaddb51039990e6825f6e8311a79817
callezenwaka <callisezenwaka@gmail.com>
2021-12-02 01:52:23 +0100

    Use props in setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,44 +1,29 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <h2>Computed</h2>
-    <!-- <p>{{ name}}</p> -->
-    <input type="text" v-model="search" id="">
-    <p>Search term: {{ search }}</p>
-    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
-    <button @click="handleclick">Stop Watch</button>
+    <h2>Props</h2>
+    <PostList :posts="posts" />
   </div>
 </template>
 
 <script>
-import { computed, ref } from '@vue/reactivity';
-import { watch, watchEffect } from '@vue/runtime-core';
+import PostList from "@/components/PostList.vue";
+import { ref } from '@vue/reactivity';
 // @ is an alias to /src
 
 export default {
   name: 'Home',
+  components: {
+    PostList
+  },
   setup() {
-    const search = ref('');
-    const names = ref(['Karl', 'Carl', 'Callis']);
-
-    const stopWatch = watch(search, () => {
-      console.log('watching search');
-    })
+    const posts = ref([
+      {title: 'Welcome home to vue', body: 'Non minim nostrud duis veniam fugiat dolore occaecat eu. Cillum culpa irure elit excepteur minim. Est culpa reprehenderit anim ad aliquip nisi ea laboris aute aliqua culpa exercitation exercitation veniam.', id: 1},
+      {title: 'Enjoy coding in vue', body: 'Irure exercitation dolor mollit aliqua. Reprehenderit fugiat ex fugiat amet velit magna sunt velit commodo aliquip sit elit excepteur consequat. Ex duis eiusmod id amet incididunt qui Lorem exercitation duis ex eu qui consectetur. Enim aliqua veniam labore anim laboris in esse pariatur.', id: 2}
+    ]);
     
-    const stopEffect = watchEffect(() => {
-      console.log('watchEffect search', search.value);
-    })
 
-    const matchingNames = computed(() => {
-      return names.value.filter((name) => name.toLowerCase().includes(search.value));
-    })
-
-    const handleclick = () => {
-      stopWatch();
-      stopEffect();
-    }
-
-    return { names, search, matchingNames, stopWatch, stopEffect, handleclick }
+    return { posts }
   }
 }
 </script>
